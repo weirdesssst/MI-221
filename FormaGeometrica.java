07c0a53ca6310fcb2218469abe28d13ff3576218
paola181 <paola.furdui@iis.utm.md>
2024-09-17 15:19:03 +0300

    Adăugat clasa Dreptunghi pentru calcularea și perimetrului dreptunghiului

---
+++ FormaGeometrica.java
@@ -0,0 +1,20 @@
+public class FormaGeometrica {
+    public static void main(String[] args) {
+        Dreptunghi dreptunghi = new Dreptunghi(4.0, 6.0);
+        System.out.println("Perimetrul dreptunghiului: " + dreptunghi.calculeazaPerimetrul());
+    }
+}
+
+class Dreptunghi extends FormaGeometrica {
+    private double lungime;
+    private double latime;
+
+    public Dreptunghi(double lungime, double latime) {
+        this.lungime = lungime;
+        this.latime = latime;
+    }
+
+    public double calculeazaPerimetrul() {
+        return 2 * (lungime + latime);
+    }
+}
