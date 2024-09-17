cc843ad6c58abfcefdfd017ee999c53621ed5538
YNeKIT <nikitascotigo@gmail.com>
2024-09-17 17:36:51 +0300

    last

--- GhostFactory.java
+++ GhostFactory.java
@@ -5,6 +5,8 @@ public class GhostFactory {
     public static Ghost getGhost(String type, String name, String color, int scareLevel) {
         if (type.equalsIgnoreCase("friendly")) {
             return new FriendlyGhost(name, color, scareLevel);
+        } else if (type.equalsIgnoreCase("enemy")) {
+            return new EnemyGhost(name, color, scareLevel);
         }
         return null; 
     }
@@ -12,7 +14,7 @@ public class GhostFactory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Introduceți tipul de fantomă (friendly): ");
+        System.out.println("Introduceți tipul de fantomă (friendly/enemy): ");
         String type = scanner.nextLine();
 
         System.out.println("Introduceți numele fantomei: ");
