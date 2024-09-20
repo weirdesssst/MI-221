6b596da80927cb2ad48c320ed039b4071ac8dcf4
Dan <danmgame0n@gmail.com>
2024-09-20 22:29:17 +0300

    Adaugare operatii in main-ul programului

--- conversieValutara.java
+++ conversieValutara.java
@@ -1,6 +1,16 @@
 public class conversieValutara {
     public static void main(String[] args) {
-        System.out.println("Urmeaza cod...");
+        // conversie din MDL in EUR
+        MDL_EURconvertor MDLinEUR = new MDL_EURconvertor();
+        MDLinEUR.afiseazaCurs();
+        double euro = MDLinEUR.converteste(1280); // converteste 1280 MDL in EUR
+        System.out.println("1280 MDL in EUR: " + euro);
+
+        // conversie din EUR in GBP
+        EUR_GBPconvertor EURinGBP = new EUR_GBPconvertor();
+        EURinGBP.afiseazaCurs();
+        double lire = EURinGBP.converteste(100); // converteste 100 EUR in GBP
+        System.out.println("100 EUR in GBP: " + lire);
     }
 }
 
