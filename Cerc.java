a56021355592a9a8020b08a858ae04a1803f2890
paola181 <paola.furdui@iis.utm.md>
2024-09-24 18:10:25 +0300

    Update Cerc.java

--- Cerc.java
+++ Cerc.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Cerc {
+class Cerc extends FormaGeometrica {
+    private double raza;
+
+    public Cerc(double raza) {
+        this.raza = raza;
+    }
+
+    public double calculeazaAria() {
+        return Math.PI * raza * raza;
+    }
 }
