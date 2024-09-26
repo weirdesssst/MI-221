157cdac8977e0260d4e66018cde09fd7a1eccc35
GabrielaTaut <gabi.taut.03@gmail.com>
2024-09-26 10:57:42 +0300

    S-a adaugat MutaLitere.java.

--- MutaLitere.java
+++ MutaLitere.java
@@ -1,18 +1,21 @@
-public class MutaLitere {
-  public String mutaLitere(String text) {
+class MutaLitere extends TextOperations {
+  public MutaLitere(String text) {
+    super(text);
+  }
+
+  @Override
+  public String operate() {
     StringBuilder rezultat = new StringBuilder();
     String[] cuvinte = text.split("\\s+");
 
     for (String cuvant : cuvinte) {
       if (cuvant.length() > 1) {
-        // Muta prima literă la final
-        String cuvantNou = cuvant.substring(1) + cuvant.charAt(0);
+        String cuvantNou = cuvant.substring(1) + cuvant.charAt(0); // Mută prima literă la final
         rezultat.append(cuvantNou).append(" ");
       } else {
-        rezultat.append(cuvant).append(" "); // Pentru cuvinte de o literă
+        rezultat.append(cuvant).append(" ");
       }
     }
-
     return rezultat.toString().trim(); // Scoate spațiul de la final
   }
 }
