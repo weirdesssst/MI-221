01a4358a32c6b4117fecb80321efff59b75e56fe
Dan <danmgame0n@gmail.com>
2024-11-18 23:29:29 +0200

    Modificare main pentru a apela functia de afisare a log-urilor pe Linux

--- Laborator_5/Main.java
+++ Laborator_5/Main.java
@@ -3,9 +3,11 @@ public class Main {
         // obtine numele sistemului de operare curent si il converteste in litere mici
         String os = System.getProperty("os.name").toLowerCase();
         
-        // verifica daca sistemul de operare este windows
+        // verifica daca sistemul de operare este windows sau linux
         if (os.contains("win")) {
-            WindowsLogReader.readLogs()
+            WindowsLogReader.readLogs();
+        } else if (os.contains("nix") || os.contains("nux")) {
+            LinuxLogReader.readLogs();
         } else {
             System.out.println("Sistem de operare neacceptat: " + os);
         }
